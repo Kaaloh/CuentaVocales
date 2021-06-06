ef56677f07b9b18fb71371213dbeaba48c6fee94
Kaaloh <charlesstone1412001@gmail.com>
2021-06-06 17:50:19 +0200

    He incluido la documentacion en el paquete, explicando lo que he hecho y para qué, y como funciona

--- CuentaVocales/src/Contador.java
+++ CuentaVocales/src/Contador.java
@@ -1,25 +1,47 @@
 
 public class Contador {
 	
+	/**
+	 * @author Carlos
+	 * 
+	 * a continuacion he creado el atributo cadena, un String y he creado el constructor con éste, y más adelante estan los getters y setters
+	 * También esta el toString
+	 * 
+	 */
 	
-	private String cadena;
+	private String cadena; 
+	
+	/**
+	 * 
+	 * @param cadena
+	 */
 	
 	public Contador(String cadena) {
 		super();
 		this.cadena = cadena;
 	}
 
+	/**
+	 * 
+	 * @param cadena
+	 * @return
+	 */
 	
 	public String numVocales(String cadena) {
 		int conteo=0;
-		for(int x=0;x<this.cadena.length();x++) {
-			 if ((this.cadena.charAt(x)=='a') || (this.cadena.charAt(x)=='e') || (this.cadena.charAt(x)=='i') || (this.cadena.charAt(x)=='o') || (this.cadena.charAt(x)=='u')){ 
+		for(int i=0;i<this.cadena.length();i++) {
+			 if ((this.cadena.charAt(i)=='a') || (this.cadena.charAt(i)=='e') || (this.cadena.charAt(i)=='i') || (this.cadena.charAt(i)=='o') || (this.cadena.charAt(i)=='u')){ 
 			    conteo++;
 			  }
 		}
 		System.out.println();
 		return"        y tiene "+conteo+" vocales";
 	}
+	/**
+	 * Funciona con un FOR que va recorriendo cada caracter de la cadena, y si un caracter es A,E,I,O,U el conteo sumará 1 hasta terminar
+	 * luego está el return que devuelve el numero de vocales de la cadena que hemos dado
+	 * @return
+	 */
 	
 
 	public String getCadena() {
@@ -31,6 +53,9 @@ public class Contador {
 	}
 
 
+	/**
+	 * en el toString he colocado el metodo numVocales, así te devuelve la cadena y además tambien el numero de vocales de la cadena
+	 */
 	@Override
 	public String toString() {
 		return "La cadena es=     " + cadena+ numVocales(this.cadena) ;
--- CuentaVocales/src/Main.java
+++ CuentaVocales/src/Main.java
@@ -1,9 +1,21 @@
 import java.util.*;
 public class Main {
 
+	/**
+	 * 
+	 * @param args
+	 * importamos el Scanner y creamos uno
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
+		
+		/**
+		 * Ahora pido que se ponga una cadena y después crea un objeto Contador con la cadena que has puesto, y lo imprimo
+		 * incluí en el toString el metodo que cuenta las vocales de forma que nos de tanto la cadena como las vocales que tiene directamente
+		 */
+		
 		
 		System.out.println("Introduzca la cadena para el conteo de vocales");
 		
